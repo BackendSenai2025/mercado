6e44788387ca835dcd8ca473c811b0d9433b507e
Leonardo Oliveira <110206541+ILeonardo-Lima@users.noreply.github.com>
2025-11-18 16:38:33 -0300

    Update FuncionarioController.java

--- src/main/java/br/com/senai/backend/sistema_mercado/controllers/FuncionarioController.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/controllers/FuncionarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.senai.backend.sistema_mercado.models.Funcionario;
 import br.com.senai.backend.sistema_mercado.models.Mercadoria;
 import br.com.senai.backend.sistema_mercado.services.FuncionarioServices;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping ("/Funcionario")

96b961be314ebed0e2650e9a82e107ab4031eb22
ILeonardo-Lima <leonardolimaoliveira5@gmail.com>
2025-11-19 02:59:26 -0300

    exclusão arquivos não nescessários

--- src/main/java/br/com/senai/backend/sistema_mercado/controllers/FuncionarioController.java
+++ src/main/java/br/com/senai/backend/sistema_mercado/controllers/FuncionarioController.java
@@ -3,10 +3,12 @@ package br.com.senai.backend.sistema_mercado.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +27,8 @@ private FuncionarioServices funcionarioServices;
 @PostMapping("/Salvar")
 public Funcionario salvar(@RequestBody Funcionario funcionario){
     return funcionarioServices.cadastrar(funcionario);
+        
 }
-
 // Get - LISTAR TODOS
 @GetMapping("/Listar Todos")
 public List<Funcionario> listarTodos(){
